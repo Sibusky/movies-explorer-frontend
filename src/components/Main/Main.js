63ed9fc90f5d9e09ebfca5543971a578df3a7184
Alexey Smirnov <Sibusky@yandex.ru>
2022-11-25 12:22:19 +0400

    feat: add <Portfolio /> layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import './Main.css'
 import AboutProject from './AboutProject/AboutProject'
 import Techs from './Techs/Techs'
 import AboutMe from './AboutMe/AboutMe'
+import Portfolio from './Portfolio/Portfolio'
 
 export default function Main() {
   return (
@@ -12,6 +13,7 @@ export default function Main() {
         <AboutProject />
         <Techs />
         <AboutMe />
+        <Portfolio />
     </div>
   )
 }
